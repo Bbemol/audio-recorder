374bff9c8239d4ce32640922702cd74623dae1d8
Benjamin PERNEY <benjperney@gmail.com>
2021-04-10 18:57:07 +0200

    Archi

---
+++ index.js
@@ -0,0 +1,56 @@
+
+
+const player = document.getElementById("player");
+const stopBtn = document.getElementById("stop");
+const recordBtn = document.getElementById("record");
+const downloadLink = document.getElementById("download");
+
+const handleSuccess = function (stream) {
+  const options = { mimeType: "audio/webm" };
+  let recordedChunks = [];
+  const mediaRecorder = new MediaRecorder(stream, options);
+  mediaRecorder.addEventListener("dataavailable", function (e) {
+    if (e.data.size > 0) {
+      recordedChunks.push(e.data);
+    }
+  });
+  stopBtn.addEventListener("click", function () {
+    mediaRecorder.stop();
+  });
+
+  mediaRecorder.addEventListener("stop", function () {
+    downloadLink.href = URL.createObjectURL(new Blob(recordedChunks));
+    downloadLink.download = "acetest.wav";
+    player.src = URL.createObjectURL(new Blob(recordedChunks));
+
+    var file = new Blob(recordedChunks, {type: 'audio/webm'});
+    var metadata = {
+        'name': 'aaaaaaa.wav', // Filename at Google Drive
+        'mimeType': 'audio/webm', // mimeType at Google Drive
+        'parents': '', // Folder ID at Google Drive
+    };
+
+    var accessToken = gapi.auth.getToken().access_token; // Here gapi is used for retrieving the access token.
+    var form = new FormData();
+    form.append('metadata', new Blob([JSON.stringify(metadata)], {type: 'application/json'}));
+    form.append('file', file);
+    var xhr = new XMLHttpRequest();
+    xhr.open('post', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id');
+    xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+    xhr.responseType = 'json';
+    xhr.onload = () => {
+        console.log(xhr.response.id); // Retrieve uploaded file ID.
+    };
+    xhr.send(form);
+
+    recordedChunks = [];
+  });
+
+  recordBtn.addEventListener("click", function () {
+    mediaRecorder.start();
+  });
+};
+
+navigator.mediaDevices
+  .getUserMedia({ audio: true, video: false })
+  .then(handleSuccess);
